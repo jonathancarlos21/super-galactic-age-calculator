b8c48400b6c751c1ee234bcb143dd397705c5869
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-02-07 13:21:49 -0800

    pass test for name.

--- __tests__/userInfo.test.js
+++ __tests__/userInfo.test.js
@@ -4,7 +4,7 @@ import {exportAllDeclaration} from '@babel/types';
 describe('UserInfo', () => {
 
   test('User enters name.', () => {
-    let newUser = new UserInfo(50);
+    let newUser = new UserInfo("James");
     expect(newUser.name).toEqual("James");
   });
 });
