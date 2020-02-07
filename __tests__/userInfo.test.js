1ac52d1538ba87d3ac565d59a77f9f7700e0447b
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-02-07 13:04:47 -0800

    add the first test for user name.

---
+++ __tests__/userInfo.test.js
@@ -0,0 +1,10 @@
+import {UserInfo} from '../src/logic';
+import {exportAllDeclaration} from '@babel/types';
+
+describe('UserInfo', () => {
+
+  test('User enters name.', () => {
+    let newUser = new UserInfo(2);
+    expect(newUser.name).equal("");
+  });
+});
---
+++ src/logic.js
@@ -0,0 +1,11 @@
+export class UserInfo {
+  constructor() {
+    this.name = "";
+    this.age = "";
+    this.lifeExpectancy = "";
+    this.planet = "";
+    this.ageOnPlanet = 0;
+    this.expectancyOnPlanet = 0;
+    this.yearsPastExpectancy = 0;
+  }
+}
